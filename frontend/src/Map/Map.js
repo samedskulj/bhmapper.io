c80ae7a1bc519d1674efd716c525f63e44888afe
samedskulj <samed.skulj08@gmail.com>
2021-08-30 20:58:32 +0200

    Added dark mode

--- frontend/src/Map/Map.js
+++ frontend/src/Map/Map.js
@@ -2,14 +2,20 @@ import React, {useState, useEffect} from 'react'
 import ReactMapGL from "react-map-gl"
 
 const Map = () => {
+
+    const bounds = [[44.3053476,19.5961467], [44.3053476, 19.5961467]];
+
     const [viewport, setViewPort] = useState({
         latitude: 44.1280,
         longitude: 18.1178,
         width: "100vw",
         height: "100vh",
-        zoom: 10
+        zoom: 15
     })
-   
+
+    const currentPosition = (position) => {
+        setViewPort({...viewport, latitude: position.coords.latitude, longitude: position.coords.longitude});
+    }
     return (
         <ReactMapGL 
         {...viewport} 
@@ -17,7 +23,10 @@ const Map = () => {
         onViewportChange={viewport => {
             setViewPort(viewport);
         }}
-        mapStyle="mapbox://styles/mapbox/light-v10"
+        mapStyle="mapbox://styles/mapbox/dark-v10"
+        maxZoom="15"
+        minZoom="7.2"
+        max
         > 
         
         </ReactMapGL>
