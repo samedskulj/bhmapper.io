486f64b4156f6d5a9568b79807daed969a8d026f
samedskulj <samed.skulj08@gmail.com>
2021-08-31 23:45:15 +0200

    Added all of the maps to change the mapbox layer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import Map from "./Map/Map";
-
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Map></Map>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Map}></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
--- frontend/src/Map/Map.js
+++ frontend/src/Map/Map.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
 import ReactMapGL from "react-map-gl";
 import mapboxgl from "mapbox-gl";
-import { bosniaX } from "./Functions/bosniaX";
 import { bosniaKulinBan } from "./Functions/bosniaKulinBan";
+import { bosniaX } from "./Functions/bosniaX";
 import "./Map.css";
+import { bosniaTvrtko } from "./Functions/bosniaTvrtko";
+import { bosniaXV } from "./Functions/bosniaXV";
+import { bosniaOttoman } from "./Functions/bosniaOttoman";
+import { bosniaAustro } from "./Functions/bosniaAustro";
+import { bosniaModern } from "./Functions/bosniaModern";
 
 const Map = () => {
   const [changeMap, setChangeMap] = useState();
@@ -26,20 +31,46 @@ const Map = () => {
     if (changeMap === "bosniax") {
       bosniaX(map);
     }
+    if (changeMap === "bosniatvrtko") {
+      bosniaTvrtko(map);
+    }
+    if (changeMap === "bosniaxv") {
+      bosniaXV(map);
+    }
+    if (changeMap === "bosniaottoman") {
+      bosniaOttoman(map);
+    }
+    if (changeMap === "bosniaaustro") {
+      bosniaAustro(map);
+    }
+    if (changeMap === "bosniamodern") {
+      bosniaModern(map);
+    }
   }, [changeMap]);
 
-  const changeBosniaX = () => {
-    setChangeMap("bosniax");
-  };
-
-  const changeBosniaKulin = () => {
-    setChangeMap("bosniakulin");
+  const changeBosnia = (bosnia) => {
+    setChangeMap(bosnia);
   };
 
   return (
     <>
-      <button onClick={changeBosniaX}>Bosnia X</button>
-      <button onClick={changeBosniaKulin}>Bosnia Kulin Ban</button>
+      <button onClick={() => changeBosnia("bosniax")}>Bosnia X</button>
+      <button onClick={() => changeBosnia("bosniakulin")}>
+        Bosnia Kulin Ban
+      </button>
+      <button onClick={() => changeBosnia("bosniatvrtko")}>
+        Bosnia Tvrtko
+      </button>
+      <button onClick={() => changeBosnia("bosniaxv")}>Bosnia XV</button>
+      <button onClick={() => changeBosnia("bosniaottoman")}>
+        Bosnia Ottoman
+      </button>
+      <button onClick={() => changeBosnia("bosniaaustro")}>
+        Bosnia Austro
+      </button>
+      <button onClick={() => changeBosnia("bosniamodern")}>
+        Bosnia Modern
+      </button>
       <div ref={mapRef} className="mapRef"></div>
     </>
   );
