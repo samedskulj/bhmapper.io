b992d02dde7d380dd5a00d02e6317c27029a0ad5
samedskulj <samed.skulj08@gmail.com>
2021-08-31 23:17:57 +0200

    Created functions to change map

--- frontend/src/Map/Map.js
+++ frontend/src/Map/Map.js
@@ -1,37 +1,48 @@
-import React, {useState, useEffect} from 'react'
-import ReactMapGL from "react-map-gl"
+import React, { useState, useEffect, useRef } from "react";
+import ReactMapGL from "react-map-gl";
+import mapboxgl from "mapbox-gl";
+import { bosniaX } from "./Functions/bosniaX";
+import { bosniaKulinBan } from "./Functions/bosniaKulinBan";
+import "./Map.css";
 
 const Map = () => {
+  const [changeMap, setChangeMap] = useState();
+  const mapRef = useRef();
 
-    const bounds = [[44.3053476,19.5961467], [44.3053476, 19.5961467]];
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-    const [viewport, setViewPort] = useState({
-        latitude: 44.1280,
-        longitude: 18.1178,
-        width: "100vw",
-        height: "100vh",
-        zoom: 15
-    })
+    const map = new mapboxgl.Map({
+      container: mapRef.current, // container ID
+      style: "mapbox://styles/mapbox/light-v10", // style URL
+      center: [17.6791, 43.9159], // starting position
+      zoom: 7, // starting zoom
+    });
 
-    const currentPosition = (position) => {
-        setViewPort({...viewport, latitude: position.coords.latitude, longitude: position.coords.longitude});
+    if (changeMap === "bosniakulin") {
+      bosniaKulinBan(map);
     }
-    return (
-        <ReactMapGL 
-        {...viewport} 
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-        onViewportChange={viewport => {
-            setViewPort(viewport);
-        }}
-        mapStyle="mapbox://styles/mapbox/dark-v10"
-        maxZoom="15"
-        minZoom="7.2"
-        max
-        > 
-        
-        </ReactMapGL>
-    ) 
-}
- 
 
-export default Map
+    if (changeMap === "bosniax") {
+      bosniaX(map);
+    }
+  }, [changeMap]);
+
+  const changeBosniaX = () => {
+    setChangeMap("bosniax");
+  };
+
+  const changeBosniaKulin = () => {
+    setChangeMap("bosniakulin");
+  };
+
+  return (
+    <>
+      <button onClick={changeBosniaX}>Bosnia X</button>
+      <button onClick={changeBosniaKulin}>Bosnia Kulin Ban</button>
+      <div ref={mapRef} className="mapRef"></div>
+    </>
+  );
+};
+
+export default Map;
