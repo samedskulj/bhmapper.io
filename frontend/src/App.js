b20597769a86e109b9edda4d1d50b000984f838e
samedskulj <samed.skulj08@gmail.com>
2021-09-01 19:26:25 +0200

    Installation of Material UI Library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import Map from "./Map/Map";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
-          <Route exact path="/" component={Map}></Route>
+          <Route exact path="/" component={Home}></Route>
         </Switch>
       </Router>
     </>
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.scss";
+import Header from "./page-components/Header";
+import MainPage from "./page-components/MainPage";
+import Footer from "./page-components/Footer";
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <MainPage />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
