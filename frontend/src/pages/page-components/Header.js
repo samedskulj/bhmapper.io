2671bcefacfa1d8240618dc4849b2165af5d4ca4
samedskulj <samed.skulj08@gmail.com>
2021-09-02 00:11:52 +0200

    Finished responsive design

--- frontend/src/pages/page-components/Header.js
+++ frontend/src/pages/page-components/Header.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
+import ReorderIcon from "@material-ui/icons/Reorder";
 const Header = () => {
+  const [showLinks, setShowLinks] = useState(true);
   return (
     <>
       <nav className="nav_bhmapper">
         <div className="nav_brand">
           <p>bhmapper</p>
         </div>
-        <div className="nav_links">
+        <div className="nav_links" id={showLinks ? "hidden" : ""}>
           <a href="/">Home</a>
           <a href="/">Portfolio</a>
           <a href="/">Contact</a>
+        </div>
+        <div onClick={() => setShowLinks(!showLinks)} className="nav_icon">
+          <ReorderIcon />
         </div>
       </nav>
     </>
